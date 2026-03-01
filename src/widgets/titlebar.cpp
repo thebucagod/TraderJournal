da83639f80a10709b579334d249d861a25eeb635
Vadim <bucagod@gmail.com>
2026-03-01 21:44:14 +0300

    feat: "mousePressEvent" method overloading
    • recognizes the click on the widget
    • fixes the coordinates

--- src/widgets/titlebar.cpp
+++ src/widgets/titlebar.cpp
@@ -11,6 +11,16 @@ titleBar::titleBar(QWidget *parent)
     setCursor(Qt::ArrowCursor);
 }
 
+// Обработка события – нажатие л.к.м. по виджету
+void titleBar::mousePressEvent(QMouseEvent* event) {
+    if((event->button() == Qt::LeftButton) && !childAt(event->pos())) { // Нажатая кнопка л.к.м. и нет ребёнка на этой позиции
+        dragging = true;
+        dragStart = event->globalPosition().toPoint() - window()->frameGeometry().topLeft();
+        event->accept();
+        return;
+    }
+    QWidget::mousePressEvent(event);
+}
 
 titleBar::~titleBar()
 {
